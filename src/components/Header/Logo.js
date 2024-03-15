9afc1779a7ec90411223d6c998eb56ca574095de
harisishar <harisishar@gmail.com>
2024-03-15 11:52:17 +0800

    update

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -8,14 +8,14 @@ const Logo = () => {
    <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
     <Image
      src={profileImg}
-     alt="Perfume malaya"
+     alt="IDW Journal"
      className="w-full h-auto rounded-full"
      sizes="20vw"
      priority
     />
    </div>
    <span className="font-bold dark:font-semibold text-lg md:text-xl">
-    Perfume Malaya
+    IDW Journal
    </span>
   </Link>
  )
